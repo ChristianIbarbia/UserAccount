c40d18ac3ea1fb5cf532e309823370524ed3ed4f
Christian <Christian@LAPTOP-ISOMLGVB>
2024-04-19 02:22:27 +0800

    Add project files.

---
+++ UserAccountUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using UserAccountBusinessServices;
+using System;
+
+namespace UserAccountUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your Username: ");
+            string Username = Console.ReadLine();
+
+            Console.WriteLine("Enter Password: ");
+            string Password = Console.ReadLine();
+
+            UserAccountService userAccountService = new UserAccountService();
+            bool result = userAccountService.VerifyUser(Username);
+
+            if (result)
+            {
+                Console.WriteLine("Succesful Login");
+
+            }
+            else
+            {
+                Console.WriteLine("Error");
+
+            }
+        }
+    }
+}
